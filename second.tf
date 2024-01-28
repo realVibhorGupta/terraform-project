0c6cf86fad659f869b5de5ea95f658250a06a5a9
Vibhor Gupta <vibhor.88.gupta@gmail.com>
2024-01-28 21:55:22 +0530

    Feat:Multiple vblocks in the tf file

---
+++ second.tf
@@ -0,0 +1,6 @@
+output "firstOutputBlock" {
+    value = "This is the  first output block hello world block"
+}
+output "secondOutputBlock" {
+    value = "This is the second output block hello world block"
+}
